d0efea5dcbc106e6a644de07f1e54e1253872cdf
Benzchanchon <chanchon1150@gmail.com>
2024-05-15 15:05:21 +0700

    te

--- flask-sql-example-main/src/views/register.py
+++ flask-sql-example-main/src/views/register.py
@@ -1,10 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect
+from sqlalchemy import select
+from ..models import Register, db
 
 bp = Blueprint("register", __name__, url_prefix="/register")
 
 
 @bp.route("", methods=["GET"])
-def register():
+def index():
     return render_template("register.html.jinja")
 
-#test
+@bp.route("", methods=["POST"])
+def create():
+    userName = request.form.get("userName")
+    gmail = request.form.get("email")
+    password = request.form.get("password")
+
+    if not id or not gmail or not password:
+        return "fail", 400
+    
+
+
+
+    return redirect (url_for("register.register"))
